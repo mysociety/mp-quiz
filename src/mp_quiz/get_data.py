14831c994231ee7beff289c22df140147737ee63
Alex Parsons <alex.parsons@mysociety.org>
2024-04-07 10:54:18 +0000

    Second release

--- src/mp_quiz/get_data.py
+++ src/mp_quiz/get_data.py
@@ -2,48 +2,93 @@ import json
 from pathlib import Path
 
 import pandas as pd
+from pydantic import BaseModel
+
+
+class CurrentMP(BaseModel):
+    person_id: int
+    name: str
+    party: str | None
+    constituency: str
+
+
+class DataDump(BaseModel):
+    current_mps: dict[int, CurrentMP]
+    current_mp_lookup: dict[str, list[int]]
+    former_mp_lookup: dict[str, list[int]]
 
 
 def create_json():
 
-    df = pd.read_parquet("https://pages.mysociety.org/politician_data/data/uk_politician_data/latest/simple_memberships.parquet")
+    df = pd.read_parquet(
+        "https://pages.mysociety.org/politician_data/data/uk_politician_data/latest/simple_memberships.parquet"
+    )
 
     # limit to chamber = commons
     # limit to end_date = 9999-12-31
 
-    df = df[(df["chamber"] == "commons") & (df["end_date"].isnull())]
+    df = df[(df["chamber"] == "commons")]
 
     df["person_id"] = df["person_id"].str.split("/").str[-1].astype(int)
 
-    good_cols = ["person_id", "nice_name", "party", "constituency"]
+    current_mp_ids = df[df["end_date"].isnull()]["person_id"]
+
+    # others is any person_id that isn't in current_mp_ids
+    other_mps_ids = df[~df["person_id"].isin(current_mp_ids)]["person_id"]
+
+    good_cols = ["person_id", "nice_name", "party", "constituency", "end_date"]
 
     df = df[good_cols]
 
     df = df.rename(columns={"nice_name": "name"})
 
-    alt_names = pd.read_parquet("https://pages.mysociety.org/politician_data/data/uk_politician_data/latest/person_alternative_names.parquet")
+    # limit to current_mps and make the currentMp lookup
+    current_mp_list = df[
+        df["person_id"].isin(current_mp_ids) & df["end_date"].isnull()
+    ].to_dict(orient="records")
+    current_mp_list = {
+        x["person_id"]: CurrentMP.model_validate(x) for x in current_mp_list
+    }
+
+    alt_names = pd.read_parquet(
+        "https://pages.mysociety.org/politician_data/data/uk_politician_data/latest/person_alternative_names.parquet"
+    )
 
     alt_names["person_id"] = alt_names["person_id"].str.split("/").str[-1].astype(int)
 
     # limit to just person_ids in df
 
-    alt_names = alt_names[alt_names["person_id"].isin(df["person_id"])]
+    alt_names = alt_names
 
     # we want to create a nice name, where it's either the 'name' column (if populated) or joining given name and family name
 
-    alt_names["nice_name"] = alt_names["name"].fillna(alt_names["given_name"] + " " + alt_names["family_name"])
+    alt_names["nice_name"] = alt_names["name"].fillna(
+        alt_names["given_name"] + " " + alt_names["family_name"]
+    )
 
-    # make a dictionary of nice_name to person_id
+    # make a dictionary of nice_name to a list of person_ids (in case multiple people share a name)
 
-    nice_name_to_person_id = alt_names.set_index("nice_name")["person_id"].to_dict()
+    nice_name_to_current_person_id = (
+        alt_names[alt_names["person_id"].isin(current_mp_ids)]
+        .groupby("nice_name")["person_id"]
+        .apply(list)
+        .to_dict()
+    )
 
-    # write to docs/data/current_mp.json each a json dictionary of person_id to the other columns in df
-    df = df.set_index("person_id")
-    df.to_json(Path("docs", "data", "current_mp.json"), orient="index")
+    nice_name_to_current_person_id["Liz Truss"] = [24941]
 
-    # write the person_id to nice_name dictionary to docs/data/person_id_to_nice_name.json
+    nice_name_to_former_person_id = (
+        alt_names[alt_names["person_id"].isin(other_mps_ids)]
+        .groupby("nice_name")["person_id"]
+        .apply(list)
+        .to_dict()
+    )
 
-    with open(Path("docs","data","nice_name_lookup.json"), "w") as f:
-        json.dump(nice_name_to_person_id, f)
+    data = DataDump(
+        current_mps=current_mp_list,
+        current_mp_lookup=nice_name_to_current_person_id,
+        former_mp_lookup=nice_name_to_former_person_id,
+    )
 
-
+    with Path("docs", "data", "mp_data.json").open("w") as f:
+        f.write(data.model_dump_json())
